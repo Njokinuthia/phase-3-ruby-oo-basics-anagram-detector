7dafafc72da83ab201f10bb4a3b33d04ffb2a5a3
Njoki <kinuthiash@gmail.com>
2022-06-12 00:07:58 +0300

    commit tree 6f2029c165ddc0cc81e81266ebe40f14e6ce22ee

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
 # Your code goes here!
+class Anagram
+  attr_reader :word, :match
+  def initialize word
+    @word = word
+  end
+
+  def match anagrams_array
+    #accepts an array as an argument
+    # loops through array and compareds each array item to word.
+    anagrams_array.each do |array_item|
+      
+       compare_word = array_item.chars.sort == @word.chars.sort      
+      
+       if compare_word == true
+        puts array_item
+       end
+      
+    end
+   
+  end
+end
+
+listen = Anagram.new "listen"
+my_array = listen.match(%w[enlists google inlets letsin banana])
+my_array
